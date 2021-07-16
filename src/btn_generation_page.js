6a6d2ab982223aeda58a1be1958166f5c753c684
Andrey13-cyber <pescherov13@gmail.com>
2021-07-16 13:30:49 +0300

    andrey

--- src/btn_generation_page.js
+++ src/btn_generation_page.js
@@ -6,6 +6,7 @@ btn_load_file.addEventListener('click', function() {
 })
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 $(document).ready(function() {
     $("#load-btn").click(function() {
         $("#editor").load("test.txt", function(){
@@ -14,9 +15,15 @@ $(document).ready(function() {
    }); 
 }); 
 =======
+=======
+>>>>>>> 20f4148df382cb1c7423587d5ca93f56d61bcc22
 let rec = document.getElementById('btn-rec')
 
 rec.addEventListener('click', function() {
     Add()
+<<<<<<< HEAD
+})
+>>>>>>> 20f4148df382cb1c7423587d5ca93f56d61bcc22
+=======
 })
 >>>>>>> 20f4148df382cb1c7423587d5ca93f56d61bcc22
